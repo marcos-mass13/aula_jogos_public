f60ae58ae55a57619d3b6c148b927ba9119fb0c2
Marcos Aurélio <39933244+marcos-mass13@users.noreply.github.com>
2025-09-10 13:22:58 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,3 @@
-const canvas = document.getElementById('idCanvas');
-const ctx = canvas.getContext('2d');
-const img2 = document.getElementById('idCanvas');
-
-canvas.width = 800;
-canvas.height = 600;
-
-let imgGoblin = 'red';
-let imgJogador = 'green';
-
 let player = new Jogador("Herói", 100, 0, 100, 70, 70, imgJogador);
 let goblin = new Inimigo("Goblin", 400, 200, 20, 70, 70, imgGoblin);
 
@@ -26,28 +16,3 @@ while (acoes.length > 0) {
         player.atacar(goblin);
     }
 }
-
-function desenhar_fase() {
-    player.desenharCor();
-    goblin.desenharCor();
-    ctx.clearRect(0, 0, ctx.width, ctx.height);
-}
-
-document.addEventListener("keydown", (event) => {
-    if (event.key === "w") {
-        player.x++;
-        console.log('moveu para cima!');
-        desenhar_fase();
-    }
-
-    if (event.key === "s") {
-        player.y++;
-        console.log('moveu para baixo!');
-        desenhar_fase();
-    }
-
-    // tente fazer para os lados (direita e esquerda)!
-})
-
-
-// setInterval(desenhar_fase, 2000);
