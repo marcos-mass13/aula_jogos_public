b3a09c581c3acea8bf0e4e02461ef8ef9950520f
Marcos Aurélio <39933244+marcos-mass13@users.noreply.github.com>
2025-09-17 12:12:51 -0300

    Update entidade.js

--- entidade.js
+++ entidade.js
@@ -1,24 +1,9 @@
 class Entidade {
-  constructor(nome, x, y, vida, width, height, img) {
+  constructor(nome, x, y, vida) {
     this.nome = nome;
     this.x = x;
     this.y = y;
     this.vida = vida;
-    this.img = img;
-    this.width = width;
-    this.height = height;
-  }
-
-  desenharCor() {
-    ctx.fillStyle = this.img;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-    console.log('cor');
-  }
-
-  desenharImg() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    console.log('img');
   }
 
   mover(dx, dy) {
