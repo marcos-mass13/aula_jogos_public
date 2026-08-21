19a2d21879c4e8da5bb1f1aa19969cfbb2fdf2bd
Marcos Aurélio <39933244+marcos-mass13@users.noreply.github.com>
2026-08-21 11:14:23 -0300

    img atte

--- hud/HudView.js
+++ hud/HudView.js
@@ -24,16 +24,13 @@ class HudView {
       const heart = document.createElement("div");
       heart.classList.add("heart");
       if (i >= this.state.hp) heart.classList.add("empty");
-      heart.innerHTML = `
-        <svg viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 
-                   2 8.5c0-3.04 2.46-5.5 5.5-5.5 
-                   1.74 0 3.41 0.81 4.5 2.09
-                   1.09-1.28 2.76-2.09 4.5-2.09
-                   3.04 0 5.5 2.46 5.5 5.5
-                   0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-        </svg>
-      `;
+       heart.innerHTML = `
+          <img 
+            src="/heart.png" 
+            alt="Coração"
+            style="width: 50px; height: 50px;"
+          >
+        `;
       this.heartsEl.appendChild(heart);
     }
   }
